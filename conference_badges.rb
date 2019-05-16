88a31a8bc806a8b980b4ea9d05e6b11428e5186e
Annie Truex <artruex@gmail.com>
2019-05-16 19:36:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,9 @@
 # Write your code here.
 def badge_maker (name)
-  "Hello my name is #{name}"
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator (attendees)
-  "Hello my name is "
+  
 end
 

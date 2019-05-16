163adf4ee7b731fd7552de503cf518e00f721bb7
Annie Truex <artruex@gmail.com>
2019-05-16 19:47:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,6 @@ def badge_maker (name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
-
-batch_badge_creator.each do |attendee|
+def batch_badge_creator(attendee)
   puts "Hello, my name is #{attendee}."
 end
